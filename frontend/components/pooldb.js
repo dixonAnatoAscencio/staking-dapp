4903803e67a195da270291d412af0cb2f0f89e30
Dixon <70873819+dixonAnatoAscencio@users.noreply.github.com>
2023-10-15 11:33:10 -0500

    add frontend nextjs

---
+++ frontend/components/pooldb.js
@@ -0,0 +1,19 @@
+
+export const poolDb = [
+    {
+        'lpToken': 'N2DR',
+        'rwdToken': 'N2DR',
+        'lpLogo':'n2dr.png',
+        'rwdLogo':'n2dr.png',
+        'autocompclass': 'btn btn-md btn-success',
+        'autocompound': 'Auto Compound',
+    },
+    {
+        'lpToken': 'USDT',
+        'rwdToken': 'N2DR',
+        'lpLogo':'usdt.png',
+        'rwdLogo':'n2dr.png',
+        'autocompclass': 'btn btn-md btn-danger',
+        'autocompound': 'Auto Compound Unavaliable',
+    },
+]
